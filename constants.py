140ec6511b3acb2c941ae79709d4d1651a911a6a
daniel13mills-max <daniel13.mills@gmail.com>
2025-12-19 09:23:39 +0000

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
-leagueID = "1609009"
-leagueStartYear = 2013
-leagueEndYear = 2024
+leagueID = "3097191"
+leagueStartYear = 2015
+leagueEndYear = 2025
 
 standings_directory = './output/'+ leagueID + '-history-standings/'
 gamecenter_directory = './output/'+ leagueID + '-history-teamgamecenter/'
